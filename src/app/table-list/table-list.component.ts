6e420e6cc4690b0a36f6ea98f823489a0d3c148a
antumin <m.vielberth@gmail.com>
2021-06-08 14:03:14 +0200

    Initial commit

---
+++ src/app/table-list/table-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-list',
+  templateUrl: './table-list.component.html',
+  styleUrls: ['./table-list.component.css']
+})
+export class TableListComponent implements OnInit {
+
+  public incidents = [
+    {id: "1", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "2", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "3", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "4", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "5", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "6", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "7", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "8", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "9", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "10", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "11", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "12", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "13", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "14", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "15", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "16", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "17", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "18", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "19", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "20", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+    {id: "21", name: "Angriff", source: "Malory", event: "ganz böses Ereignis", entities: "ganze blockchain", impact: "alles kaputt"},
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
