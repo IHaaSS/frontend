a81688eb3079e0eaf35bdd3cabd9cc28defaf638
antumin <m.vielberth@gmail.com>
2021-07-06 14:17:46 +0200

    Add answer service

---
+++ src/app/services/answer.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnswerService {
+  private myAnswer = new BehaviorSubject<any>(null);
+  // answerUrl = 'http://127.0.0.1:5000/answer'
+  private answerUrl = environment.baseUrl + '/answer';
+
+  constructor(private http: HttpClient) { }
+
+  sendAnswer(answer: any): void {
+    console.log('sendAnswer');
+    console.log(answer);
+    this.myAnswer.next(answer);
+  }
+
+  getAnswer(): Observable<any> {
+    return this.myAnswer.asObservable();
+  }
+
+  postAnswer(answer: Answer) {
+    console.log('postAnswer');
+    return this.http.post(this.answerUrl, JSON.stringify(answer)).pipe(
+      catchError(this.handleError<Answer>('postAnswer')));
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+}
+
+export class Answer {
+  id: number;
+  phase: number;
+  answers: Question[];
+}
+
+export class Question {
+  attributeId: number;
+  value: number;
+  topicId: number;
+}
